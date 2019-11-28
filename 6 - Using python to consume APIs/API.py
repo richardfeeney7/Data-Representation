eba5654b1b17dba2972499438f944ca08c615ae0
Richard Feeney <richardfeeney7@gmail.com>
2019-11-28 03:27:34 +0000

    Lab  06.01:  Use  Python  to  access  Server  

--- 6 - Using python to consume APIs/API.py
+++ 6 - Using python to consume APIs/API.py
@@ -40,6 +40,7 @@ for car in data["cars"]:
  response = requests.post(url, json=dataString) 
  print (response.status_code)
 
+
  import requests import json 
  dataString = {'make':'Ford','model':'Kuga'} url = 'http://127.0.0.1:5000/cars/test' 
  response = requests.put(url, json=dataString) 
